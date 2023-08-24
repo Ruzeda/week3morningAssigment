69511ed51aa549332c713e542ab728c1facf5481
Ruzeda Fields <fields.rozay@gmail.com>
2023-08-23 19:58:18 -0700

    finished

---
+++ index.js
@@ -0,0 +1,36 @@
+function greeting() {
+  console.log("Hello World!");
+}
+
+function addNumbers(num1, num2) {
+  return num1 + num2;
+}
+
+function checkEvenOrOdd(num) {
+    if (num % 2 === 0) {
+      console.log(num + " is even.");
+    } else {
+      console.log(num + " is odd.");
+    }
+  }
+
+function isPrime(num) {
+    if (num <= 1) {
+      return false;
+    }
+    for (let i = 2; i < num; i++) {
+      if (num % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+function factorial(num) {
+    if (num === 0) {
+      return 1;
+    }
+    return num * factorial(num - 1);
+  }
+  
+  
